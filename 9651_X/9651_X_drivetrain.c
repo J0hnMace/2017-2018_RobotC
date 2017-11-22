082d5250f02cbf1030e5478ac8a9d789901d6159
9651Robotics <9651robotics@gmail.com>
2017-11-22 15:29:49 -0500

    sorted out the errors
    missing ";" after "while(bSoundActive)", -temporarily commented out

--- 9651_X/9651_X_drivetrain.c
+++ 9651_X/9651_X_drivetrain.c
@@ -14,23 +14,20 @@
 
 #include "Vex_Competition_Includes.c"
 
-/* calibrate sensors, deactivate first law  */
+/* calibrate sensors, deactivate first law */
 void pre_auton()
 {
-		/*  */
+	/*  */
 }
 
 /* autonomous period */
 task autonomous()
 {
-	while(1 == 1)
-		{
-			/* Plays X Gon Give It To Ya (clean) */
-			bPlaySounds = true;
-			playSoundFile("9651x_ggity.wav");
-			playSoundFile("9651x_ggity");
-			while(bSoundActive)
-		}
+	/* code below may need to be uncommented*/
+	//bPlaySounds = true;
+	playSoundFile("9651x_ggity.wav");
+	//playSoundFile("9651x_ggity");
+	//while(bSoundActive);
 }
 
 /* driver control period */
@@ -40,37 +37,37 @@ task usercontrol()
 	int Y1 = 0, X1 = 0, threshold = 30;
 
 	while(1 == 1)
-		{
-			/* create deadzones */
-			if(abs(vexRT[AccelY]) > threshold)
+	{
+		/* create deadzones */
+		if(abs(vexRT[AccelY]) > threshold)
 			Y1 = vexRT[AccelY]^2/127;
-			else
+		else
 			Y1 = 0;
-			if(abs(vexRT[AccelX]) > threshold)
+		if(abs(vexRT[AccelX]) > threshold)
 			X1 = vexRT[AccelX]^2/127;
-			else
+		else
 			X1 = 0;
 
-			/* mecanum drive, Y1=forward/reverse, X1=turn */
-			motor[frontRight] = - Y1 - X1 - vexRT[Ch4];
-    	motor[backRight] = - Y1 - X1 + vexRT[Ch4];
-    	motor[frontLeft] = - Y1 + X1 + vexRT[Ch4];
-    	motor[backLeft] = - Y1 + X1 - vexRT[Ch4];
+		/* mecanum drive, Y1=forward/reverse, X1=turn */
+		motor[frontRight] = - Y1 - X1 - vexRT[Ch4];
+		motor[backRight] = - Y1 - X1 + vexRT[Ch4];
+		motor[frontLeft] = - Y1 + X1 + vexRT[Ch4];
+		motor[backLeft] = - Y1 + X1 - vexRT[Ch4];
 
-    		/* mobile goal lift */
-  			if(vexRT[Btn5D] == 1)
-  				{motor[goal] = -127;}
-  			else if(vexRT[Btn5U] == 1)
-  				{motor[goal] = 127;}
-    		else
-    			{motor[goal] = 0;}
+		/* mobile goal lift */
+		if(vexRT[Btn5D] == 1)
+		{motor[goal] = -127;}
+		else if(vexRT[Btn5U] == 1)
+		{motor[goal] = 127;}
+		else
+		{motor[goal] = 0;}
 
-    		/* left side cone lift */
-    		if(vexRT[Btn7U] == 1)
-    			{motor[coneLeft] = -127;}
-    		else if(vexRT[Btn7D] == 1)
-    			{motor[coneLeft] = 127;}
-    		else
-    			{motor[coneLeft] = 0;}
-    	}
+		/* left side cone lift */
+		if(vexRT[Btn7U] == 1)
+		{motor[coneLeft] = -127;}
+		else if(vexRT[Btn7D] == 1)
+		{motor[coneLeft] = 127;}
+		else
+		{motor[coneLeft] = 0;}
+	}
 }
